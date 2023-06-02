0e105580ac97ae034778b835be9ccde678b974a9
Nicol Guevara <111058276+Neofitar@users.noreply.github.com>
2023-06-01 22:04:08 -0500

    Formulario

--- src/components/formNuevaCategoria/index.js
+++ src/components/formNuevaCategoria/index.js
@@ -1,99 +1,99 @@
-import "../formNuevoVideo/formulario.css"
-import CampoInput from "../campo-inputs"
-import CampoTextarea from "../campo-textarea"
-import BotonAzul from "../boton-azul"
-import BotonBlanco from "../boton-blanco"
-import { useState } from "react"
+import "../formNuevoVideo/formulario.css";
+import React from 'react';
+import CampoInput from "../campo-inputs";
+import CampoTextarea from "../campo-textarea";
+import BotonAzul from "../boton-azul";
+import BotonBlanco from "../boton-blanco";
+import { useState } from "react";
+import TablaCategorias from "../tablaCategorias";
 
 const NuevaCategoria = (props) => {
 
-    const [categoria, setCategoria] = useState({
-        nombre: '',
-        descripcion: '',
-        color: '',
-        codigoSeguridad: '',
+  const [categoria, setCategoria] = useState({
+    titulo: '',
+    descripcion: '',
+    color: '',
+    codigoSeguridad: '',
+  });
+
+  const limpiarFormulario = () => {
+    setCategoria({
+      titulo: '',
+      descripcion: '',
+      color: '',
+      codigoSeguridad: '',
     });
+  };
 
-    const limpiarFormulario = () => {
-        setCategoria({
-            nombre: '',
-            descripcion: '',
-            color: '',
-            codigoSeguridad: '',
-        });
-    };
+  const handleChange = (event, campo) => {
+    setCategoria({ ...categoria, [campo]: event.target.value });
+  };
 
-    const handleChange = (event, campo) => {
-        const { value } = event.target;
-        setCategoria((preCaregoria) => ({
-            ...preCaregoria,
-            [campo]: value,
-        }));
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(categoria);
+    limpiarFormulario();
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(categoria);
-        limpiarFormulario();
-    };
-
-
-    return <section className="formulario-nuevo">
-        <h2>Nuevo categoría</h2>
-        <form onSubmit={handleSubmit}>
-            <CampoInput
-                title="Nombre:"
-                valor={categoria.nombre}
-                actualizarValor={(event) => handleChange(event, "nombre")}
-                required
+  return (
+    <section className="formulario-nuevo">
+      <h2>Nueva categoría</h2>
+      <form onSubmit={handleSubmit}>
+        <CampoInput
+          title="Nombre:"
+          valor={categoria.titulo}
+          actualizarValor={(event) => handleChange(event, "titulo")}
+          required
+        />
+        <CampoTextarea
+          title="Descripción:"
+          valor={categoria.descripcion}
+          actualizarValor={(event) => handleChange(event, "descripcion")}
+          required
+        />
+        <CampoInput
+          title="Color:"
+          type="color"
+          required
+          valor={categoria.color}
+          actualizarValor={(event) => handleChange(event, "color")}
+        />
+        <CampoInput
+          title="Código de seguridad:"
+          valor={categoria.codigoSeguridad}
+          actualizarValor={(event) => handleChange(event, "codigoSeguridad")}
+          required
+        />
+        <div className="botones">
+          <div className="botones-add">
+            <BotonAzul id="ck" type="submit" title="Guardar"></BotonAzul>
+            <BotonBlanco title="Limpiar" onClick={limpiarFormulario}></BotonBlanco>
+          </div>
+        </div>
+      </form>
+      <table className="tabla-categorias">
+        <thead>
+          <tr className="tabla-cabecera">
+            <th className="td-titulo">Nombre</th>
+            <th className="td-descripcion">Descripción</th>
+            <th className="td-btn">Color</th>
+            <th className="td-btn">Editar</th>
+            <th className="td-btn">Remover</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.grupos && props.grupos.map((categoria) => (
+            <TablaCategorias
+              title={categoria.titulo}
+              descripcion={categoria.descripcion}
+              color={categoria.color}
+              key={categoria.id}
             />
-            <CampoTextarea
-                title="Descripción:"
-                valor={categoria.descripcion}
-                actualizarValor={(event) => handleChange (event, "descripcion")}
-                required
-            />
-            <CampoInput
-                title="Color:"
-                type="color"
-                required
-                valor={categoria.color}
-                actualizarValor={(event) => handleChange (event, "color")}
-            />
-            <CampoInput
-                title="Código de seguridad:"
-                valor={categoria.codigoSeguridad}
-                actualizarValor={(event) => handleChange (event, "codigoSeguridad")}
-                required
-            />
-            <div className="botones">
-                <div className="botones-add">
-                    <BotonAzul id="ck" type="submit" title="Guardar"></BotonAzul>
-                    <BotonBlanco title="Limpiar" onClick={limpiarFormulario}></BotonBlanco>
-                </div>
-            </div>
-        </form>
-
-        <table className="tabla-categorias">
-            <tr className="tabla-cabecera">
-                <td>Nombre</td>
-                <td>Descripción</td>
-                <td>Editar</td>
-                <td>Remover</td>
-            </tr>
-            <tr>
-                <td>Back End</td>
-                <td>Todos los video que estoy usando para estudiar Back End </td>
-                <td>
-                    <button className="button-edit">Editar</button>
-                </td>
-                <td>
-                    <button className="button-delete">Remover</button>
-                </td>
-            </tr>
-        </table>
-
+          ))}
+        </tbody>
+      </table>
     </section>
-}
+  );
+};
 
-export default NuevaCategoria
+export default NuevaCategoria;
--- src/db/categoriasDB.js
+++ src/db/categoriasDB.js
@@ -2,47 +2,63 @@ const grupos = [
     {   
         id: "001",
         titulo: "Programación",
-        color: "#6BD1FF"
+        color: "#6BD1FF",
+        descripcion: "Todos los video que estoy usando para estudiar Programación."
     },
     {   
         id: "002",
         titulo: "Back End",
-        color: "#00C86F"
+        color: "#00C86F",
+        descripcion: "Todos los video que estoy usando para estudiar Back End."
     },
     {
         id: "003",
         titulo: "Front End",
-        color: "#FFBA05"
+        color: "#FFBA05",
+        descripcion: "Todos los video que estoy usando para estudiar Front End."
+
     },
     {
         id: "004",
         titulo: "Móvil",
-        color: "#DC6EBE"
+        color: "#DC6EBE",
+        descripcion: "Todos los video que estoy usando para estudiar Móvil."
+
     },
     {
         id: "005",
         titulo: "Análisis de datos",
-        color: "#9CD33B"
+        color: "#9CD33B",
+        descripcion: "Todos los video que estoy usando para estudiar Análisis de datos."
+
     },
     {   
         id: "006",
         titulo: "Data Science",
-        color: "#6B5BE2"
+        color: "#6B5BE2",
+        descripcion: "Todos los video que estoy usando para estudiar Data Science."
+
     },
     {
         id: "007",
         titulo: "Devops",
-        color: "#FF8C2A"
+        color: "#FF8C2A",
+        descripcion: "Todos los video que estoy usando para estudiar Devops."
+
     },
     {
         id: "008",
         titulo: "Innovación y Gestión",
-        color: "#9CD33B"
+        color: "#9CD33B",
+        descripcion: "Todos los video que estoy usando para estudiar Innovación y Gestión."
+
     },
     {
         id: "009",
         titulo: "UX y Diseño",
-        color: "#6BD1FF"
+        color: "#6BD1FF",
+        descripcion: "Todos los video que estoy usando para estudiar UX y Diseño."
+
     }
 ];
 
